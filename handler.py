3b50d760e5defc200fcb5e95fe0ae09ff98cdb9c
LucasLeRay <lucas.a.leray@gmail.com>
2021-02-20 15:01:35 +0100

    feat: extract, transform and load tweets

--- handler.py
+++ handler.py
@@ -1,24 +1,51 @@
-import json
+import os
+import tweepy as tw
+import datetime
 
+from sqlalchemy import create_engine, MetaData, Table, Column, String
+
+DB_CONN = os.environ.get('DB_CONN')
+TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+TWITTER_ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+TWITTER_ACCESS_SECRET = os.environ.get('TWITTER_ACCESS_SECRET')
+
+metadata = MetaData()
+
+sample_table = Table('sample_tweets', metadata,
+    Column('id', String(100), primary_key=True),
+    Column('text', String(300), nullable=False),
+    Column('created_at', String(20), nullable=False),
+)
+
+def setupAPI():
+    auth = tw.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+    return tw.API(auth)
+
+def getData(query, limit, lang='fr'):
+    api = setupAPI()
+    return tw.Cursor(api.search, q=query, lang=lang).items(limit)
+
+def cleanup(item):
+    return {
+        'id': item.id_str,
+        'text': item.text,
+        'created_at': item.created_at.strftime("%d/%m/%Y")
+    }
+
+def init_tables(engine):
+    sample_table.drop(engine, checkfirst=True)
+    metadata.create_all(engine)
 
 def main(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    data = getData('Macron', 5)
+    cleaned_data = [cleanup(item) for item in data]
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    engine = create_engine(DB_CONN)
+    init_tables(engine)
+    conn = engine.connect()
+    conn.execute(sample_table.insert().values(cleaned_data))
+    
+    return cleaned_data
 
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
